ca6613c919af892899c07228498e440a6bf30a9f
Matt Woodruff <mwoodruff@rfxsolutions.com>
2023-11-21 20:41:26 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# What is fresh-store?
+
+Fresh store is a very simple store that allows islands to communicate using
+hooks.
+
+# Design Docs
+
+<div style="text-align: center;">
+  <img src="./store-design.svg"  />
+</div>
---
+++ mod.ts
@@ -0,0 +1,38 @@
+import { useSignal } from "@preact/signals";
+
+export const useStore = <T>(uniqName: string, defaultState: () => T) => {
+  const state = useSignal<T>(defaultState());
+
+  const handleStateChangeEvent = (event: Event) => {
+    const customEvent = event as CustomEvent<T>;
+    state.value = customEvent.detail;
+  };
+
+  globalThis.window.addEventListener(uniqName, handleStateChangeEvent);
+
+  const setState = (state: T) => {
+    const event = new CustomEvent<T>(uniqName, {
+      detail: state,
+    });
+
+    globalThis.window.dispatchEvent(event);
+  };
+
+  const updateState = (partial: Partial<T>) => {
+    setState({
+      ...state.value,
+      ...partial,
+    });
+  };
+
+  const resetState = () => {
+    setState(defaultState());
+  };
+
+  return {
+    state,
+    setState,
+    updateState,
+    resetState,
+  };
+};
